36bc79de65af57a5eb89b2e06a26a1e8ddb45bbd
DavidMilai <davidmilai67@gmail.com>
2021-11-18 17:06:19 +0300

    users service and db

--- lib/data/database.dart
+++ lib/data/database.dart
@@ -2,16 +2,20 @@ import 'package:hive/hive.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 import 'models/auth_data.dart';
+import 'models/user.dart';
 
 class Database {
   late Box<AuthData> authDataBox; //typeId: 0
+  late Box<User> users; //typeId: 1
 
   _initBoxes() async {
     authDataBox = await Hive.openBox("authData");
+    users = await Hive.openBox("users");
   }
 
   _registerAdapters() async {
     Hive.registerAdapter(AuthDataAdapter());
+    Hive.registerAdapter(UserAdapter());
   }
 
   init() async {
